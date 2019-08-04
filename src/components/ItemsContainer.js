4de42cde96347b24311efbfbe1af1a88f338b5a8
Eddie Chu <contact@eddiechu.me>
2019-08-04 02:05:29 -0700

    add animation

--- src/components/ItemsContainer.js
+++ src/components/ItemsContainer.js
@@ -5,7 +5,7 @@ export default class ItemsContainer extends React.Component {
 
     render() {
         let itemsList = this.props.data.map( (item, index) => {
-            return <Item data={item} key={index} />
+            return <Item data={item} key={index} style={{ animationDelay: `${index / 10}s` }}/>
         });
 
         return <div className="items-container">{itemsList}</div>;
