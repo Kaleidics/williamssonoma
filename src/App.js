9ef04ba54917b9fc3ed3d8190d4b627b45f597fd
Eddie Chu <contact@eddiechu.me>
2019-08-04 23:09:00 -0700

    prettier

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import seed from './seed';
-import ItemsContainer from './components/ItemsContainer'
+import React from "react";
+import seed from "./seed";
+import ItemsContainer from "./components/ItemsContainer";
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      items: []
+        this.state = {
+            items: []
+        };
     }
-  }
 
-  //sets the seed data from seed file to the state
-  componentDidMount() {
-    this.setState({
-      items: seed.groups
-    })
-  }
+    //sets the seed data from seed file to the state
+    componentDidMount() {
+        this.setState({
+            items: seed.groups
+        });
+    }
 
-  render() {
-    console.log(this.state.items);
-    return (
-    <div className="App" >
-        <ItemsContainer data={this.state.items} />
-    </div>
-    );
-  }
+    render() {
+        console.log(this.state.items);
+        return (
+            <div className="App">
+                <ItemsContainer data={this.state.items} />
+            </div>
+        );
+    }
 }
